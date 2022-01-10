cff0fa6900d1685d9132175d9e7274db8797630f
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-01-11 00:47:50 +0100

    Implemented Tests for MyCollection
    Implemented Tests for MyCollection

--- AB4/projekt/src/test/java/at/aau/ue4/bsp2/MyCollectionTest.java
+++ AB4/projekt/src/test/java/at/aau/ue4/bsp2/MyCollectionTest.java
@@ -4,11 +4,10 @@ package at.aau.ue4.bsp2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-public class MyCollectionTest
-{
+public class MyCollectionTest {
     private MyCollection c;
 
     @BeforeEach
@@ -18,8 +17,42 @@ public class MyCollectionTest
         c.add("2");
         c.add("3");
     }
+
     @Test
     public void testSizeSimple() {
-        assertEquals(3,c.size());
+        assertEquals(3, c.size());
     }
+
+    @Test
+    public void shouldIncrease_whenAddAddsNewItem(){
+        c.add("4");
+        assertEquals(4,c.size());
+    }
+
+    @Test
+    public void shouldThrow_whenListIsFull(){
+        c.add("4");
+        c.add("5");
+        assertThrows(IllegalArgumentException.class, ()->c.add("Full"));
+    }
+
+    @Test
+    public void shouldDecrease_whenItemIsRemoved(){
+        assertEquals(3,c.size());
+        c.remove("1");
+        assertEquals(2,c.size());
+    }
+
+    @Test
+    public void shouldThrow_whenAttemptingToRemoveItemFromEmptyCollection(){
+        assertThrows(IllegalArgumentException.class, ()->c.remove("1"));
+    }
+
+    @Test
+    public void shouldBeSizeZero_whenEmptied(){
+        assertEquals(3,c.size());
+        c.empty();
+        assertEquals(0, c.size());
+    }
+
 }
