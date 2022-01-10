d0e8048648cf6784adb420186359b6e32a26b4bc
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-01-10 15:18:29 +0100

    Upload Mac

--- AB3/project/src/test/java/at/aau/ue3/bsp2/RelationCheckerTest.java
+++ AB3/project/src/test/java/at/aau/ue3/bsp2/RelationCheckerTest.java
@@ -1,5 +1,15 @@
 package at.aau.ue3.bsp2;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RelationCheckerTest {
     //TODO fill tests
+
+    @Test
+    public void ifXandYIsZero_ThenReturnZero(){
+        Assert.assertEquals(0, RelationChecker.checkRelation(0,0));
+
+
+    }
 }
