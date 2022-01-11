5655d9e058ece0d1038baa30c11d92d375428678
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-01-11 01:37:16 +0100

    Implemented McCabe tests
    Implemented 3 basistests

--- AB4/projekt/src/test/java/at/aau/ue4/bsp3/McCabeTest.java
+++ AB4/projekt/src/test/java/at/aau/ue4/bsp3/McCabeTest.java
@@ -1,8 +1,22 @@
 package at.aau.ue4.bsp3;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class McCabeTest {
     @Test
-    public void test() {}
+    public void basistest1() {
+        assertEquals(1, McCabe.ggt(1,0));
+    }
+
+    @Test
+    public void basistest2(){
+        assertEquals(3, McCabe.ggt(9,3));
+    }
+
+    @Test
+    public void basistest3(){
+        assertEquals(1, McCabe.ggt(5,32));
+    }
+
 }
