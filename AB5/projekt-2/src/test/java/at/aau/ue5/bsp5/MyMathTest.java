00db2e4fd1da63192f831317792621843d1a51dd
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-01-22 20:09:54 +0100

    Refactored MyMathTest
    Long Method, Duplicated Code and feature envy

--- AB5/projekt-2/src/test/java/at/aau/ue5/bsp5/MyMathTest.java
+++ AB5/projekt-2/src/test/java/at/aau/ue5/bsp5/MyMathTest.java
@@ -1,25 +1,88 @@
 package at.aau.ue5.bsp5;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.DoubleBuffer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyMathTest {
 
-    @Test
-    public void x() {
-        Double x=10d;
-        Double y=5d;
-        Double add=new MyMath().add(x,y);
-        Double sub=new MyMath().sub(x,y);
-        Double mul=new MyMath().mul(x,y);
-        Double div=new MyMath().div(x,y);
+    private MyMath myMath;
+    private Double x;
+    private Double y;
+    private Fraction fraction;
 
-        assertTrue(add.equals(new Double(15)));
-        assertTrue(sub.equals(new Double(5)));
-        assertTrue(mul.equals(new Double(50)));
-        assertTrue(div.equals(new Double(2)));
+    @BeforeEach
+    public void setup(){
+        myMath = new MyMath();
+        x = 10d;
+        y = 5d;
+        fraction = new Fraction();
+    }
+
+    @Test
+    public void shouldReturnFifteen_WhenAddingTenAndFive(){
+        assertEquals(15,(double)myMath.add(x,y));
+    }
+
+    @Test
+    public void shouldReturnFive_WhenTenMinusFive(){
+        assertEquals(5,(double)myMath.sub(x,y));
+    }
+
+    @Test
+    public void shouldReturnFifty_WhenMultiplyingTenAndFive(){
+        assertEquals(50,(double)myMath.mul(x,y));
+    }
+
+    @Test
+    public void shouldReturnTwo_WhenDividingTenByFive(){
+        assertEquals(2,(double)myMath.div(x,y));
+    }
+
+    @Test
+    public void shouldReturnOneAndOne_WhenInputIsOneAndOne(){
+        setNumAndDenum(1,1);
+
+        assertEquals(new Fraction(1,1), myMath.reduce(fraction));
+    }
+
+    @Test
+    public void shouldReturnFiveAndThree_WhenInputIsTenAndSix(){
+        setNumAndDenum(10,6);
+
+        assertEquals(new Fraction(5,3),myMath.reduce(fraction));
+    }
+
+    @Test
+    public void shouldReturn2And1_WhenInputIs10And5(){
+        setNumAndDenum(10,5);
+
+        assertEquals(new Fraction(2,1),myMath.reduce(fraction));
+    }
+
+    @Test
+    public void shouldReturn2d_WhenInputIs10And5(){
+        setNumAndDenum(10,5);
+
+        assertEquals(2, (double)myMath.toDouble(fraction));
+    }
+
+    @Test
+    public void shouldReturn2dot5_WhenInputIs10And4(){
+        setNumAndDenum(10,4);
+
+        assertEquals(2.5, (double)myMath.toDouble(fraction));
+    }
+
+    @Test
+    public void shouldReturnDoubleNan_WhenDividedBy0(){
+        setNumAndDenum(10,0);
+
+        assertEquals(Double.NaN ,(double)myMath.div(10d,0d));
     }
 
     @Test
@@ -51,5 +114,10 @@ public class MyMathTest {
         mm = new MyMath();
         aDouble = mm.toDouble(f);
         assertEquals(new Double(2.5d),aDouble);
+    }
+
+    public void setNumAndDenum(int numerator, int denumerator){
+        this.fraction.setNumerator(numerator);
+        this.fraction.setDenumerator(denumerator);
     }
 }
